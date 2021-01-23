2ed7525e011f8d00edd5e5962f5847221fe80eb6
jdcar <jdcarlstone@gmail.com>
2021-01-23 10:59:08 -0600

    server set up

--- server.js
+++ server.js
@@ -2,10 +2,46 @@ const express = require ('express')
 const app = express()
 const PORT = 3000
 
+
+const characters = [
+    {
+        name: 'Yoda',
+        role: 'Jedi Master',
+        forcePoints: 100000,
+        age: 900, 
+        avatar: 'https://images.app.goo.gl/YwNw3ijVo95eYe5S6',
+        routeName: 'yoda'
+    },
+    {
+        name: 'Luke Skywalker',
+        role: 'Jedi Master',
+        forcePoints: 10000,
+        age: 40, 
+        avatar: 'https://images.app.goo.gl/M3Kv4xFfmzFo6cdi8',
+        routeName: 'lukeskywalker'
+    },
+    {
+        name: 'Princess Leia',
+        role: 'Princess General',
+        forcePoints: 100,
+        age: 40, 
+        avatar: 'https://images.app.goo.gl/9S8E6XPf4iYSiNs99',
+        routeName: 'princessleia'
+    }
+
+]
 app.get('/', (req, res) => {
     res.send('May the force be with you')
 
 })
+
+// /api/characters - show all char data
+
+app.get('/api/characters', (req, res) => {
+    res.json(characters)
+})
+
+// /api/characters/:routename
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`)
